eaeaf2d8c7ec6a68eda01d11c80037f1ba9b2dbf
Sarah Leong <sleong20@stuy.edu>
2021-11-23 16:24:24 -0500

    put the redirect  code back alskdjfaslkdjf

--- Chespin.c
+++ Chespin.c
@@ -31,6 +31,89 @@ char ** parse_args( char * line ){
 
 
 /***
+  Changes the current working directory
+    param: parsed command ["cd", "path"]
+    returns: errno
+***/
+
+// it prints an error whenever I use it but it actually changes the directory so it's weird
+int cd(char ** args){
+
+// if the user entered an absolute path from the home directory
+  if(args[1][0] == '~'){
+    //gets home directory of the user
+    char * path = getenv("HOME");
+    //concate home path with user's entered path
+    strcat(path, ++args[1]);
+    printf("path: %s\n", path);
+    chdir(path);
+  }else{
+    chdir(args[1]);
+  }
+
+  return errno;
+}
+
+
+char ** redirect_helper(int fd, int std, int * out, char ** temp){
+
+  dup2(fd,std);
+  *temp = NULL;
+  temp[1] = NULL;
+  temp+=2;
+  out[0] = std;
+
+  return temp;
+}
+
+/***
+Redirect to files if the commands contains instructions regarding redirection
+  params: parsed commands
+  return: file descriptor of the replaced, whether stdin or stdout is replaced [0 (stdin) or 1 (stdout), file descriptor of its location]
+***/
+
+int * redirect(char ** args){
+  char ** temp = args;
+
+  while(*temp && temp){
+      int * out = malloc(2*sizeof(int));
+    if (strcmp(*temp,">") == 0){
+      //printf("stdout\n");
+      int stdout = dup(1);
+      int fd = open(temp[1], O_CREAT|O_TRUNC|O_WRONLY, 0644);
+      temp = redirect_helper(fd,1,out,temp);
+      out[1] = stdout;
+      return out;
+    }else if (strcmp(*temp,">>")==0){
+      //printf("append\n");
+      int stdout = dup(1);
+      int fd = open(temp[1], O_CREAT|O_APPEND|O_WRONLY, 0644);
+      temp = redirect_helper(fd,1,out,temp);
+      out [1] = stdout;
+      return out;
+    }else if (strcmp(*temp, "<")==0){
+      //printf("stdin\n");
+      int stdin = dup(0);
+      int fd = open(temp[1],O_RDONLY);
+      temp = redirect_helper(fd,0,out,temp);
+      out[1] = stdin;
+      return out;
+    }
+
+
+    temp++;
+  }
+  int * out = malloc(2*sizeof(int));
+  out[0] = -1;
+  out [1] = -1;
+  return out;
+}
+/***
+reads and executes commands
+  return: errno if there is an error, otherwise 0.
+***/
+
+/***
 
 reads and executes commands
   return: errno if there is an error, otherwise 0.
@@ -51,34 +134,45 @@ fgets(line, 100, stdin);
 
 //parse arguments
 char ** args = parse_args( line );
-char ** comms = args;
+char ** comms = sizeof(args);
 int args_ind = 0;
 int comms_ind = 0;
 
-// printf("%d\n", strcmp(comms[comms_ind],";"));
-while (comms[comms_ind] && strcmp(comms[comms_ind],";")) {
-  comms[comms_ind] = args[args_ind];
-  printf("%s", comms[comms_ind]);
+while (args[args_ind]) {
+  // printf("%s", comms[comms_ind]);
+  if (strcmp(args[args_ind],";")) {
+    comms[comms_ind] = args[args_ind];
+    printf("copying %s into comms\n",args[args_ind]);
+  }
   args_ind++;
   comms_ind++;
+
+  if (!comms[comms_ind]) {
+    if(strcmp(comms[0], "exit")==0){
+      exit(0);
+    }
+
+    //initiate child process
+    int child1 = fork();
+
+    if (child1) {
+      w = wait(&status);
+      // printf("child %d finished; parent %d resumes\n",child1,getpid());
+    }
+    else if (!child1){
+      // printf("pid child: %d\tparent: %d\n", getpid(),getppid());
+      //execute commands
+      printf("%s\n",comms[0]);
+      execvp(comms[0], comms);
+      // exit(0);
+    }
+    comms = args;
+    comms_ind = 0;
+    args_ind++;
+  }
+
+
 }
 
-if(strcmp(comms[0], "exit")==0){
-  exit(0);
-}
-
-//initiate child process
-int child1 = fork();
-
-if (child1) {
-  w = wait(&status);
-  // printf("child %d finished; parent %d resumes\n",child1,getpid());
-}
-else if (!child1){
-  // printf("pid child: %d\tparent: %d\n", getpid(),getppid());
-  //execute commands
-  execvp(comms[0], comms);
-  // exit(0);
-}
  return 0;
 }
